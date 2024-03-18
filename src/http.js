98fcddc062ddd6b97c85b2a719d9099b4db9bf2f
Assaf <1214assaf@gmail.com>
2024-03-18 16:56:05 +0200

    sending data with post request

--- src/http.js
+++ src/http.js
@@ -7,3 +7,18 @@ export async function fetchAvailablePlaces() {
 
   return responseData.places;
 }
+
+export async function updateUserPlaceS(places) {
+  response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error(resData.message || "Failed to update user data.");
+  }
+
+  return resData.message;
+}
